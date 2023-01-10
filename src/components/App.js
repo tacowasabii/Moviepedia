acad7c1b5f6c9317f7faafd066696adb40e6103d
JUHYEON LEE <godseoril812@gmail.com>
2023-01-10 18:31:16 +0900

    filter로 아이템 삭제하기

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,20 @@
 import { useState } from "react";
 import ReviewList from "./ReviewList";
-import items from "../mock.json";
+import mockItems from "../mock.json";
 
 function App() {
   const [order, setOrder] = useState("createdAt");
+  const [items, setItems] = useState(mockItems);
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
   const handleNewestClick = () => setOrder("createdAt");
 
   const handleBestClick = () => setOrder("rating");
+
+  const handleDelete = (id) => {
+    const nextItems = items.filter((item) => item.id !== id);
+    setItems(nextItems);
+  };
 
   return (
     <div>
@@ -16,7 +22,7 @@ function App() {
         <button onClick={handleNewestClick}>최신순</button>
         <button onClick={handleBestClick}>베스트순</button>
       </div>
-      <ReviewList items={sortedItems} />
+      <ReviewList items={sortedItems} onDelete={handleDelete} />
     </div>
   );
 }
