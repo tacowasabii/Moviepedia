7d1c2c213cf02332d12300b3ef59c5c51b4d7f91
JUHYEON LEE <godseoril812@gmail.com>
2023-01-10 19:01:31 +0900

    ref로 DOM 노드 가져오기

--- src/components/FileInput.js
+++ src/components/FileInput.js
@@ -1,10 +1,14 @@
+import { useEffect, useRef } from "react";
+
 function FileInput({ name, value, onChange }) {
+  const inputRef = useRef();
+
   const handleChange = (e) => {
     const nextValue = e.target.files[0];
     onChange(name, nextValue);
   };
 
-  return <input type="file" onChange={handleChange} />;
+  return <input type="file" onChange={handleChange} ref={inputRef} />;
 }
 
 export default FileInput;
