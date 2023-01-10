2e385ca6c125535eb326e3d975d5020d2f5a2569
JUHYEON LEE <godseoril812@gmail.com>
2023-01-10 19:36:17 +0900

    다국어 기능 완성하기

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import useAsync from "../hooks/useAsync";
+import useTranslate from "../hooks/useTranslate";
 import FileInput from "./FileInput";
 import RatingInput from "./RatingInput";
 import "./ReviewForm.css";
@@ -18,6 +19,7 @@ function ReviewForm({
   onSubmit,
   onSubmitSuccess,
 }) {
+  const t = useTranslate();
   const [values, setValues] = useState(initialValues);
   const [isSubmitting, submittingError, onSubmitAsync] = useAsync(onSubmit);
 
@@ -69,9 +71,9 @@ function ReviewForm({
         value={values.content}
         onChange={handleInputChange}
       />
-      {onCancel && <button onClick={onCancel}>취소</button>}
+      {onCancel && <button onClick={onCancel}>{t("cancel button")}</button>}
       <button disabled={isSubmitting} type="submit">
-        확인
+        {t("confirm button")}
       </button>
       {submittingError && <div>{submittingError.message}</div>}
     </form>
--- src/components/ReviewList.js
+++ src/components/ReviewList.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useLocale } from "../contexts/LocaleContext";
+import useTranslate from "../hooks/useTranslate";
 import Rating from "./Rating";
 import ReviewForm from "./ReviewForm";
 import "./ReviewList.css";
@@ -10,7 +10,7 @@ function formatDate(value) {
 }
 
 function ReviewListItem({ item, onDelete, onEdit }) {
-  const locale = useLocale();
+  const t = useTranslate();
 
   const handleDeleteClick = () => {
     onDelete(item.id);
@@ -28,9 +28,8 @@ function ReviewListItem({ item, onDelete, onEdit }) {
         <Rating value={item.rating} />
         <p>{formatDate(item.createdAt)}</p>
         <p>{item.content}</p>
-        <p>현재 언어: {locale}</p>
-        <button onClick={handleEditClick}>수정</button>
-        <button onClick={handleDeleteClick}>삭제</button>
+        <button onClick={handleEditClick}>{t("edit button")}</button>
+        <button onClick={handleDeleteClick}>{t("delete button")}</button>
       </div>
     </div>
   );
