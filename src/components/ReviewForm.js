0eedf3c310374694d00c4fdc7382e75998344207
JUHYEON LEE <godseoril812@gmail.com>
2023-01-10 19:12:01 +0900

    리스폰스 데이터 반영하기

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -11,7 +11,7 @@ const INITIAL_VALUES = {
   imgFile: null,
 };
 
-function ReviewForm() {
+function ReviewForm({ onSubmitSuccess }) {
   const [values, setValues] = useState(INITIAL_VALUES);
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [submittingError, setSubmittingError] = useState(null);
@@ -35,17 +35,20 @@ function ReviewForm() {
     formData.append("rating", values.rating);
     formData.append("content", values.content);
     formData.append("imgFile", values.imgFile);
+    let result;
     try {
       setSubmittingError(null);
       setIsSubmitting(true);
-      await createReview(formData);
+      result = await createReview(formData);
     } catch (error) {
       setSubmittingError(error);
       return;
     } finally {
       setIsSubmitting(false);
     }
+    const { review } = result;
     setValues(INITIAL_VALUES);
+    onSubmitSuccess(review);
   };
 
   return (
