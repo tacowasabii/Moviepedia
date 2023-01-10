996dcd55307268b4a3bd33c651292e32e1d51657
JUHYEON LEE <godseoril812@gmail.com>
2023-01-10 18:54:22 +0900

    onSubmit 추가

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -19,11 +19,21 @@ function ReviewForm() {
     setContent(e.target.value);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({
+      title,
+      rating,
+      content,
+    });
+  };
+
   return (
-    <form className="ReviewForm">
+    <form className="ReviewForm" onSubmit={handleSubmit}>
       <input value={title} onChange={handleTitleChange} />
       <input type="number" value={rating} onChange={handleRatingChange} />
       <textarea value={content} onChange={handleContentChange} />
+      <button type="submit">확인</button>
     </form>
   );
 }
