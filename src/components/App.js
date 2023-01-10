3ba39ef6eb8132095b1ed38ad8704917db9e5160
JUHYEON LEE <godseoril812@gmail.com>
2023-01-10 19:22:23 +0900

    글 삭제하기

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import ReviewList from "./ReviewList";
 import ReviewForm from "./ReviewForm";
-import { createReview, getReviews, updateReview } from "../api";
+import { createReview, deleteReview, getReviews, updateReview } from "../api";
 
 const LIMIT = 6;
 
@@ -18,9 +18,11 @@ function App() {
 
   const handleBestClick = () => setOrder("rating");
 
-  const handleDelete = (id) => {
-    const nextItems = items.filter((item) => item.id !== id);
-    setItems(nextItems);
+  const handleDelete = async (id) => {
+    const result = await deleteReview(id);
+    if (!result) return;
+
+    setItems((prevItems) => prevItems.filter((item) => item.id !== id));
   };
 
   const handleLoad = async (options) => {
